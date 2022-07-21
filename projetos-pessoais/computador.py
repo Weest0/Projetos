88b794c270435f0db1297938be88cfad2e217e97
Weest0 <wesleyusumaki910@gmail.com>
2022-07-21 16:11:26 -0300

    projetos-pessoais

---
+++ projetos-pessoais/computador.py
@@ -0,0 +1,86 @@
+print(' \n ▀█▀ ▒█▀▀█ ▒█▀▄▀█ 　 ░░ 　 ▒█▀▀▀█ ▀█▀ ▒█▀▀▀█ ▀▀█▀▀ ▒█▀▀▀ ▒█▀▄▀█ \n ▒█░ ▒█▀▀▄ ▒█▒█▒█ 　 ▀▀ 　 ░▀▀▀▄▄ ▒█░ ░▀▀▀▄▄ ░▒█░░ ▒█▀▀▀ ▒█▒█▒█ \n ▄█▄ ▒█▄▄█ ▒█░░▒█ 　 ░░ 　 ▒█▄▄▄█ ▄█▄ ▒█▄▄▄█ ░▒█░░ ▒█▄▄▄ ▒█░░▒█ \n')
+
+print(' BEM VINDO!! \n\n run - help (para saber todos os comandos.)')
+
+while(True):
+	controle = 0
+
+	comm=str(input('\n=>> '))
+	
+	if ( comm == 'help'): #ajuda
+		print('\ncreate - cria um arquivo.\ncreatept - cria uma pasta.\ncalc - para chamar a calculadora. \nconfig - acessa as configurações do sistema.')
+	
+	elif (comm == 'calc'): #calculadora!!!!!!
+		
+		pri=float(input('\nPrimeiro valor: '))
+		seg=float(input('Segundo valor: '))
+		
+		print('\n(+ para soma.)\n(- para subtração.)\n(* para multiplicação.)\n(/ para divisão.)')
+		ope=str(input('\nInsira o operador: '))
+		
+		if (ope == '+'):
+			soma=pri+seg
+			print(f'\nResultado: {soma:.2f}')
+		elif (ope == '-'):
+			sub=pri-seg
+			print(f'\nResultado: {sub:.2f}')
+		elif (ope == '*'):
+			mult=pri*seg
+			print(f'\nResultado: {mult:.2f}')
+		elif (ope == '/'):
+			div=pri/seg
+			print(f'\nResultado: {div:.2f}')
+		elif(ope != '+' or '-' or '*' or '/'):
+			print('\n--------|X|---------\n\nERROR!!\nTENTE NOVAMENTE:')
+			controle=1
+
+		while (controle == 1):
+			print('\n(+ para soma.)\n(- para subtração.)\n(* para multiplicação.)\n(/ para divisão.)')
+			ope=str(input('\nInsira o operador: '))
+			
+			if (ope == '+'):
+				soma=pri+seg
+				print(f'\nResultado: {soma:.2f}')
+				break
+			elif (ope == '-'):
+				sub=pri-seg
+				print(f'\nResultado: {sub:.2f}')
+				break
+			elif (ope == '*'):
+				mult=pri*seg
+				print(f'\nResultado: {mult:.2f}')
+				break
+			elif (ope == '/'):
+				div=pri/seg
+				print(f'\nResultado: {div:.2f}')
+				break
+			elif(ope != '+' or '-' or '*' or '/'):
+				print('\n--------|X|---------\n\nERROR!!\nTENTE NOVAMENTE:')
+
+	elif(comm == 'config'):
+		while(True):
+			conf_1='Sim'
+			print('\n-------------------------------------\nConfirme para salvar as configurações.\n-------------------------------------\nSelecione um número para entrar na configuração \n-------------------------------------\nexit - para sair \n-------------------------------------\n\n   [1] - Configurações de senha. \n'.format(conf_1))
+			conf=str(input('\n(config)=>> '))
+
+			if(conf == '1'):
+				while(True):
+					print('\nSeção [Senhas].\n [1] - Iniciar com senha: {}'.format(conf_1))
+					conf=str(input('\n(config-senhas)=>> '))
+					if(conf == 'exit'):
+						break
+			
+			if(conf == 'exit'):
+				print('\nSalve antes de sair. [S|N]')
+				conf=str(input('\nSalvar?\n\n(config)==> '))
+				
+				if(conf == 'S'):
+					print('\nSALVO')
+					break
+				elif(conf == 'N'):
+					print('\nOk')
+					break
+
+	elif(comm == 'exit'): #sair
+		break
+print('\nBye')
---
+++ projetos-pessoais/tabela.py
@@ -0,0 +1,14 @@
+var = str(input('=>> '))
+var2 = str('-')
+var3 = str('|')
+var4 = str('+')
+var5 = str(' ')
+cont = var.count('')
+
+print(var4,var2*(cont-1),var4,var2*(cont-1),var4)
+print(var3,var,var3,var5*(cont-1),var3)
+print(var4,var2*(cont-1),var4,var2*(cont-1),var4)
+
+#print((var2[:cont] + '') * cont * cont )
+#print((var3[:cont] + var)* cont)
+#print((var2[:cont] + '') * cont * cont)
